a3271469304b583b04df08a0032003e68ba4e260
Norman Koch <norman.koch@tu-dresden.de>
2020-04-21 12:37:56 +0200

    installer

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 echo "This script installs VVZ. IT HAS ONLY BEEN TESTED ON DEBIAN"
 
+echo "THIS DOES NOT QUITE WORK YET. DO IT MANUALLY"
+exit 1
+
 read -p "Where to put VVZ: " maindir
 read -p "Name of the database: " dbname
 read -p "DB Pass (user: root): " dbpass
 
 mkdir -p $maindir
+
+function run_code_on_db {
+	echo "Running $1 on DB"
+	mysql -hlocalhost -uroot -p$dbpass -e "$1"
+}
 
 echo "$dbpass" > /etc/vvzdbpw
 
@@ -19,6 +27,10 @@ git clone --depth 1 https://github.com/NormanTUD/VVZ.git .
 
 sed --in-place "s/PLACEHOLDERDBNAME/$dbname/" mysql.php
 
-mysql -hlocalhost -uroot -p$dbpass -e "create database ${dbname}"
+sudo systemctl restart mysql
+run_code_on_db "UPDATE user SET Password=PASSWORD('$dbpass') where USER='root';"
+run_code_on_db "FLUSH PRIVILEGES;"
+sudo systemctl restart mysql
+run_code_on_db "create database ${dbname}"
 
 echo "Open the URL of this host in your browser to finish the setup"
