a2915aa02194abe770d179f0883c1eb68de3518a
Norman Koch <norman.koch@tu-dresden.de>
2020-09-24 18:00:35 +0200

    Institute nur dann anzeigen wenn welche eingerichtet sind

--- pages/accounts.php
+++ pages/accounts.php
@@ -22,7 +22,13 @@
 				<th>Passwort</th>
 				<th>Rolle</th>
 				<th>Zugeordneter Dozent</th>
-				<th>Institut</th>
+<?php
+				if(count($instituten) > 1) {
+?>
+					<th>Institut</th>
+<?php
+				}
+?>
 				<th>Barriere&shy;freier Zu&shy;gang*</th>
 				<th>Daten&shy;schutz&shy;frage ak&shy;zep&shy;tiert</th>
 				<th>Speichern</th>
@@ -42,7 +48,17 @@
 						<td><input type="password" name="password" value="" placeholder="passwort" /></td>
 						<td><?php create_select($rollen, $row[2], 'role'); ?></td>
 						<td><?php create_select($dozenten, $row[3], 'dozent', $row[3] ? 0 : 1); ?></td>
-						<td><?php create_select($instituten, $row[4], 'institut', $row[4] ? 0 : 1); ?></td>
+<?php
+						if(count($instituten) > 1) {
+?>
+							<td><?php create_select($instituten, $row[4], 'institut', $row[4] ? 0 : 1); ?></td>
+<?php
+						} else {
+?>
+							<input type="hidden" name="institut" value="<?php print $instituten[1][0]; ?>" />
+<?php
+						}
+?>
 						<td><input type="checkbox" name="barrierefrei" value="1" <?php print $row[6] == 1 ? 'checked="CHECKED"' : ''; ?>/></td>
 						<td><input type="checkbox" name="accepted_public_data" value="1" <?php print $row[7] == 1 ? 'checked="CHECKED"' : ''; ?>/></td>
 						<td><input type="submit" value="Speichern" /></td>
@@ -71,7 +87,17 @@
 					<td><input type="password" name="password" value="" placeholder="passwort" /></td>
 					<td><?php create_select($rollen, 2, 'role'); ?></td>
 					<td><?php create_select($dozenten, '', 'dozent', 0); ?></td>
-					<td><?php create_select($instituten, '', 'institut', 0); ?></td>
+<?php
+					if(count($instituten) > 1) {
+?>
+						<td><?php create_select($instituten, '', 'institut', 0); ?></td>
+<?php
+					} else {
+?>
+						<input type="hidden" name="institut" value="<?php print $instituten[1][0]; ?>" />
+<?php
+					}
+?>
 					<td><input type="checkbox" name="barrierefrei" value="1" /></td>
 					<td><input type="checkbox" name="accepted_public_data" disabled="disabled" /></td>
 					<td><input type="submit" value="Speichern" /></td>
