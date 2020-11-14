4ff335b765db753459adf939ac5e3c8be1c3744b
Norman Koch <norman.koch@tu-dresden.de>
2020-11-14 23:46:35 +0100

    raumplanung_export_2 (neue version) geaddet

---
+++ raumplanung_export_2.php
@@ -0,0 +1,21 @@
+<?php
+	$setup_mode = 0;
+	include_once("functions.php");
+
+	if(check_page_rights(get_page_id_by_filename('raumplanung.php'))) { // Wichtig, damit Niemand ohne Anmeldung etwas ändern kann
+		if(get_get('semester')) {
+			$semester_name = get_semester(get_get('semester'));
+			$institut_name = get_institut_name(get_get('institut'));
+			header('Content-type: application/vnd.ms-excel');
+			header('Content-Disposition: attachment; filename="crazy-Raumplanung-'.$institut_name.'-'.$semester_name[2].'_'.$semester_name[1].'.xlsx"');
+
+			$objPHPExcel = raumplanung_crazy(get_get('institut'), get_get('semester'), 0);
+			$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+			$objWriter->save('php://output');
+		} else {
+			die("Der Parameter `semester` muss mit einem validen Semester definiert worden sein.");
+		}
+	} else {
+		die("Sie dürfen auf diese Seite nicht zugreifen.");
+	}
+?>
