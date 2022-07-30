4295badc99f10b7baa85b220ba095e0bc5659702
Norman VM1 <vm1@optimalbliss.de>
2022-07-31 01:03:37 +0200

    correctly load header...

--- header.php
+++ header.php
@@ -4,6 +4,13 @@
 	}
 	include_once("functions.php");
 
+	if(file_exists('new_setup')) {
+		if(!isset($GLOBALS["setup_mode"])) {
+			include('setup.php');
+			exit(0);
+		}
+	}
+
 	if($GLOBALS['reload_page']) {
 		header("Refresh:0");
 	}
@@ -12,7 +19,7 @@
 		$page_title = 'Vorlesungsverzeichnis '.$GLOBALS['university_name'];
 	}
 
-	if(get_get("studiengang")) {
+	if(get_get("studiengang") && !$GLOBALS["setup_mode"]) {
 		$page_title = "$page_title | ".get_studiengang_name(get_get('studiengang'));
 	}
 
