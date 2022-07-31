631f60a115cc59ddf5041d5d25f2a8335b3b83c7
root <root@vvz-test.local>
2022-07-31 11:59:41 +0200

    installer

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ fi
 INSTALL_PATH=/var/www/html
 INSTITUT_NAME="Institut für Philosophie"
 
-sudo apt-get update
-sudo apt-get install xterm whiptail sudo curl git -y
+apt-get update
+apt-get install xterm whiptail curl git -y
 
 eval `resize`
 
@@ -30,27 +30,28 @@ if [ -d "$INSTALL_PATH" ]; then
 	mv "$INSTALL_PATH" "$MOVE_TO"
 fi
 
+mkdir -p $INSTALL_PATH
 cd $INSTALL_PATH
 
 git clone --depth 1 https://github.com/NormanTUD/VVZ.git .
 
 function apt_get_upgrade {
-	sudo apt-get upgrade -y
+	apt-get upgrade -y
 }
 
 function install_apache {
-	sudo apt-get install curl unzip ca-certificates apt-transport-https lsb-release gnupg apache2 -y
+	apt-get install curl unzip ca-certificates apt-transport-https lsb-release gnupg apache2 -y
 }
 
 function install_php {
 	wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add - && \
 	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
-	sudo apt-get update && \
-	sudo apt-get install php8.1 php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-intl php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-xml php8.1-xsl php8.1-zip php8.1-bz2 libapache2-mod-php8.1 -y
+	apt-get update && \
+	apt-get install php8.1 php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-intl php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-xml php8.1-xsl php8.1-zip php8.1-bz2 libapache2-mod-php8.1 -y
 }
 
 function install_mariadb {
-	sudo apt install mariadb-server mariadb-client -y
+	apt install mariadb-server mariadb-client -y
 }
 
 function setup_mariadb {
@@ -66,9 +67,7 @@ EOF
 }
 
 function copy_to_path {
-	sudo mv $INSTALL_PATH /var/www/old_html
-	sudo mkdir -p $INSTALL_PATH
-	sudo cp -r * $INSTALL_PATH
+	cp -r * $INSTALL_PATH
 }
 
 function new_setup {
