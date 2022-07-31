5c1a9c552282156e8c764d890e48a5d45e089c40
Norman Koch <norman.koch@tu-dresden.de>
2022-07-31 12:57:46 +0200

    removed old line

--- install.sh
+++ install.sh
@@ -77,8 +77,6 @@ function install_mariadb {
 }
 
 function setup_mariadb {
-	#mysql_secure_installation
-
 	mysql -u root <<-EOF
 UPDATE mysql.user SET Password=PASSWORD('$PASSWORD') WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
