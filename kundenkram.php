b02b103bde0e3819eeec10160decc667ea6ddabd
Norman VM1 <vm1@optimalbliss.de>
2022-08-23 00:30:35 +0200

    fixed bug

--- kundenkram.php
+++ kundenkram.php
@@ -44,7 +44,7 @@
 
 	function get_kunde_url() {
 		$n = get_kunde_name();
-		if(array_key_exists('REQUEST_URI', $_SERVER) && preg_match("/^\Q".$n."\E/", $_SERVER['REQUEST_URI']) || preg_match("/^\/vvz_".$n."/", $_SERVER["REQUEST_URI"])) {
+		if(array_key_exists('REQUEST_URI', $_SERVER) && preg_match("/^\Q".$n."\E/", $_SERVER['REQUEST_URI']) || preg_match("/^vvz_".$n."/", $_SERVER["REQUEST_URI"])) {
 			return "";
 		} else {
 			return "vvz_$n/";
