467646bd70d674f91e87d31b0134305fa0506896
Norman VM1 <vm1@optimalbliss.de>
2022-08-25 19:27:05 +0200

    bla?

--- startseite.php
+++ startseite.php
@@ -172,7 +172,7 @@
 				</div>
 			</div>
 <?php
-			$page_str = "<center><h2>Aktuelle Kunden:</h2>";
+			$page_str = "<center><h2>Aktuelle Kunden:</h2></center>";
 			$page_str .= "<ul class='side_by_side'>";
 			$str_contents = "";
 			while ($row = mysqli_fetch_row($result)) {
@@ -200,7 +200,7 @@
 				print $page_str;
 				print $str_contents;
 				print "</ul>";
-				print "</div></div></center>";
+				print "</div></div>";
 			}
 ?>
 
