cc19fa9fa3293e01c71699779cc7e990f2720298
vvz-test <kochnorman@rocketmail.com>
2022-08-27 16:41:14 +0200

    sticky save

--- pages/veranstaltung.php
+++ pages/veranstaltung.php
@@ -76,7 +76,7 @@
 				include_once('hinweise.php');
 ?>
 				<form method="post" action="admin?page=<?php print $GLOBALS['this_page_number']; ?>&id=<?php print $veranstaltung_id; ?>">
-					<input type="submit" class="text_30px" value="Speichern" style="position: sticky; top: 10px;" />
+					<input type="submit" class="text_30px sticky_save" value="Speichern" />
 
 					<table>
 						<tr>
