5fe676c3f6f121ab8007f39b199568dbee54cb69
vvz-test <kochnorman@rocketmail.com>
2022-08-28 21:49:54 +0200

    warning -> message

--- admin.php
+++ admin.php
@@ -74,7 +74,7 @@
 			} else {
 				$fehler .= "Bitten Sie einen Administrator, Studiengänge hinzuzufügen.";
 			}
-			warning($fehler);
+			message($fehler);
 		}
 
 
@@ -86,7 +86,7 @@
 			} else {
 				$fehler .= "Bitten Sie einen Administrator, Module hinzuzufügen.";
 			}
-			warning($fehler);
+			message($fehler);
 		}
 
 		$module = create_modul_array();
@@ -97,7 +97,7 @@
 			} else {
 				$fehler .= "Bitten Sie einen Administrator, Module hinzuzufügen.";
 			}
-			warning($fehler);
+			message($fehler);
 		}
 
 		$gebaeude = create_gebaeude_array();
@@ -108,7 +108,7 @@
 			} else {
 				$fehler .= "Bitten Sie einen Administrator, Module hinzuzufügen.";
 			}
-			warning($fehler);
+			message($fehler);
 		}
 
 		$pruefungsnummern = create_pruefungsnummern_array();
@@ -119,7 +119,7 @@
 			} else {
 				$fehler .= "Bitten Sie einen Administrator, Module hinzuzufügen.";
 			}
-			warning($fehler);
+			message($fehler);
 		}
 
 		if(get_get('make_all_foreign_keys_on_delete_cascade') == 1) {
