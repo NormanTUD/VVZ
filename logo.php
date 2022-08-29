1649347a6c9a2320f835948a33a1a97930e91b2a
debian <kochnorman@rocketmail.com>
2022-08-29 19:22:41 +0200

    logo from db

--- logo.php
+++ logo.php
@@ -1,4 +1,18 @@
 <?php
+	include_once("config.php");
+	include_once("kundenkram.php");
+	include_once("functions.php");
+	include_once("selftest.php");
+
 	header("Content-type: image/png");
-	readfile("default_logo.png");
+
+	$query = "select img from vvz_global.logos where kunde_id = ".esc(get_kunde_id_by_db_name(get_kunden_db_name()));
+	$result = get_single_row_from_query($query);
+
+	if($result) {
+		print $result;
+	} else {
+
+		readfile("default_logo.png");
+	}
 ?>
