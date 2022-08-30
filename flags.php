7683d708e673519db648dbd7cc1ada7ae4bed493
debian <kochnorman@rocketmail.com>
2022-08-30 11:51:27 +0200

    flags übersicht

---
+++ flags.php
@@ -0,0 +1,32 @@
+<?php
+	$flags = array(
+		array("always_accept_public_data" => "Automatisch die Datenschutzabfrage akzeptieren (NUR FÜR DEVELOPER!)"),
+		array("api_debug", "Debugging für die API"),
+		array("hardcore_debugging", "Stirbt sofort bei jedem Fehler, jede Warning ist sofort fatal."),
+		array("vvz_comic_sans", "Easter-Egg aktivieren"),
+		array("vvz_debug_query", "Zeige den Query-Debugger"),
+		array("vvz_debug_query_all", "Zeige alle Queries im Query-Debugger"),
+		array("vvz_snow", "Easter Egg (geht aber nicht)"),
+		array("x11test", "Deaktiviert einige Sachen, um automatisches X11-Testen einfacher zu machen.")
+	);
+?>
+	<table>
+		<tr>
+			<th>Dateipfad</th>
+			<th>Beschreibung</th>
+			<th>Aktiv?</th>
+		</tr>
+<?php
+		foreach ($flags as $row) {
+			if($row[0]) {
+?>
+				<tr>
+					<td>/etc/<?php print $row[0]; ?></td>
+					<td><?php print $row[1]; ?></td>
+					<td><?php print file_exists("/etc/".$row[0]) ? '&#9989;' : '&#10060;'; ?></td>
+				</tr>
+<?php
+			}
+		}
+?>
+	</table>
