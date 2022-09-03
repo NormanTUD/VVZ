2f149d936cc79d896b21140e65b3aa7971dc8a6d
debian <kochnorman@rocketmail.com>
2022-09-04 01:52:23 +0200

    licences

---
+++ license.php
@@ -0,0 +1,106 @@
+<h1>§ 1 Vertragsgegenstand</h1>
+
+<p>(1) Vertragsgegenstand ist die Überlassung von Software.</p>
+
+<p>(2) Der Anbieter stellt dem Kunden während der Laufzeit dieses Vertrages die Nutzung der Software 'vorlesungsverzeichn.is' zu eigenen Zwecken zur Verfügung.</p>
+
+<p>(3) Zugriff und Nutzung der auf Servern des Anbieters gespeicherter Software erfolgen über eine Internetverbindung durch die Verwendung eines Internet-Browsers. Die Software ist über folgende Webseite erreichbar: <?php print htmlentities($_SERVER['HTTP_HOST'] ?? ""); ?></p>
+
+<p>(4) Zu den wesentlichen vertraglichen Funktionen der Software gehören Folgende:</p>
+
+<p>(5) Der Anbieter bietet dem Kunden die Software stets in der aktuellsten Version an.</p>
+
+<p>(6) Da die Software laufend weiterentwickelt und aktualisiert wird, garantieren wir keine spezifische Uptime. Wir testen jedoch automatisch, ob der Service noch verfügbar ist, und reparieren ihn so schnell es geht.</p>
+
+<p>(7) Während des Aktualisierungsvorgangs wird der Anbieter von seiner Vertragspflicht aus § 1 Abs. 2 dieses Vertrages befreit.</p>
+
+<h1>§ 2 Beginn und Laufzeit des Nutzungsverhältnisses</h1>
+
+<p>(1) Dieser Vertrag tritt mit Unterzeichnung in Kraft und endet mit einer formlosen Kündigungsemail.</p>
+
+<h1>§ 3 Vergütung</h1>
+
+<p>(1) Im Gegenzug zu den Leistungen des Anbieters hat der Kunde eine entsprechende Vergütung zu erbringen.</p>
+
+<p>(2) Die Vergütung erfolgt unabhängig vom genutzten Volumen.</p>
+
+<h1>§ 4 Verfügbarkeit der Software</h1>
+
+<p>(1) Der Anbieter weist den Kunden darauf hin, dass er keine 100%ige Verfügbarkeit der Software garantieren kann, wenn Einschränkungen oder Beeinträchtigungen entstehen, die außerhalb des Einflussbereichs des Anbieters stehen. Der Anbieter kann auch außerhalb der Fälle des § 1 Abs. 6 und 7 dieses Vertrages mit Zustimmung des Kunden für einen bestimmten Zeitraum von seiner Leistungspflicht befreit werden.</p>
+
+<p>(2) Der Kunde ist verpflichtet, den Anbieter unverzüglich schriftlich (per Brief oder per E-Mail) darüber zu unterrichten, sobald die Software nicht verfügbar ist.</p>
+
+<h1>§ 5 Nutzungsrecht des Kunden, Zugriffsberechtigung</h1>
+
+<p>(1) Der Kunde erhält an der Software ein auf die Laufzeit des vorliegenden Vertrages beschränktes Nutzungsrecht.</p>
+
+<p>(2) Es erfolgt keine körperliche Überlassung der Software. Die Software bleibt jederzeit auf dem Server des Anbieters.</p>
+
+<h1>§ 6 Schulung</h1>
+
+<p>Der Anbieter bietet kostenfreie Schulungsvideos zu allen relevanten Themen. Bei Bedarf können Sie uns kontaktieren und wir versuchen, neue Schulungsvideos zu erstellen.</p>
+
+<h1>§ 7 Support</h1>
+
+<p>Der Anbieter stellt dem Kunden zur Beseitigung von technischen Störungen und Behebung von Fehlern, die im Rahmen der Nutzung der Software aufkommen, telefonisch einen Kundendienst zur Verfügung. Der Kundendienst des Anbieters ist via Email erreichbar, und zwar folgendermaßen:</p>
+
+<p>Email: noack@omni-concept.com</p>
+
+
+<h1>§ 8 Mängelansprüche</h1>
+
+<p>(1) Der Anbieter haftet für Mängel der Vertragsleistungen.</p>
+
+<p>(2) Ansprüche nach § 536a BGB, insbesondere die verschuldensunabhängige Garantiehaftung und das Selbstvornahmerecht betreffend, sind ausgeschlossen.</p>
+
+<p>(3) Ein Sachmangel liegt vor, wenn die Software nicht die vertraglich vereinbarte Beschaffenheit aufweist oder sich nicht für die vertraglich vorausgesetzte Verwendung eignet. Unerhebliche Abweichungen stellen keinen Mangel dar.</p>
+
+<p>(4) Der Kunde ist verpflichtet, den Anbieter unverzüglich schriftlich (per Brief oder per E-Mail) von aufgetretenen Mängeln zu unterrichten.</p>
+
+
+<h1>§ 9 Haftung</h1>
+
+<p>(1) Die Vertragsparteien haften für Vorsatz und grobe Fahrlässigkeit.</p>
+
+<p>(2) Der Anbieter haftet für die Verletzung wesentlicher Vertragspflichten (sog. Kardinalspflichten). Dabei handelt es sich um solche vertraglichen Pflichten, deren Erfüllung den Vertrag so wesentlich prägt, als dass deren Verletzung eine Gefährdung der Erreichung des Vertragszwecks darstellt, und auf deren Einhaltung der Kunde vertrauen darf. Soweit die Kardinalspflichten fahrlässig verletzt wurden, ist der Schadensersatzanspruch des Kunden begrenzt auf den vertragstypisch vorhersehbaren Schaden.</p>
+
+<p>(3) Der Anbieter haftet außerdem gemäß den gesetzlichen Bestimmungen nach den Vorschriften des Produkthaftungsgesetzes und für Schäden, die durch die Verletzung des Lebens, des Körpers oder der Gesundheit des Kunden entstanden sind.</p>
+
+<p>(4) Der Anbieter haftet für Schäden seiner Erfüllungsgehilfen.</p>
+
+<h1>§ 10 Herausgabe und Löschung von Daten</h1>
+
+<p>(1) Nach Beendigung des Vertragsverhältnisses hat der Anbieter sämtliche Daten, Unterlagen und Datenträger des Kunden, die der Anbieter im Zusammenhang mit diesem Vertrag erhalten hat für den Kunden zum Download bereitgestellt werden.</p>
+
+<p>(2) Der Anbieter hat innerhalb von 48 Stunden nach Beendigung des Vertragsverhältnisses sämtliche gespeicherte Daten des Kunden auf dem eigenen Server vollständig zu löschen.</p>
+
+<h1>§ 11 Geheimhaltung, Vertraulichkeit</h1>
+
+<p>(1) Die Parteien sind verpflichtet, alle ihnen im Zusammenhang mit diesem Vertrag bekannt gewordenen vertraulichen Informationen über die jeweils andere Partei dauerhaft geheim zu halten, nicht an Dritte weiterzugeben, aufzuzeichnen oder in anderer Weise zu verwerten, sofern die jeweils andere Partei der Offenlegung oder Verwendung nicht ausdrücklich und schriftlich zugestimmt hat oder die Informationen aufgrund Gesetzes, Gerichtsentscheidung oder Verwaltungsentscheidung offengelegt werden müssen. Liegt keine solche Zustimmung oder Offenlegung vor, sind die bekannt gewordenen Informationen nur zur Durchführung dieses Vertrages zu verwenden.</p>
+
+<p>(2) Der Kunde ist insbesondere zur Geheimhaltung hinsichtlich aller Inhalte der Software verpflichtet. Der Kunde darf die Zugriffsdaten (Benutzernamen und Passwörter) nicht an Unbefugte weitergeben.</p>
+
+<p>(3) Keine vertraulichen Informationen im Sinne des § 13 Abs. 1 dieses Vertrages sind Folgende:</p>
+
+<p>    Informationen, die der anderen Partei bereits zuvor bekannt waren.</p>
+<p>    Informationen, die allgemein bekannt sind.</p>
+<p>    Informationen, die der anderen Partei von einem Dritten offenbart wurden, ohne dass dieser dadurch eine Vertraulichkeitsverpflichtung verletzt hat.</p>
+
+<p>(4) Die Verpflichtungen aus diesem Paragraphen sind auch auf den Zeitraum nach Beendigung des Vertragsverhältnisses anzuwenden.</p>
+
+<h1>§ 12 Schlussbestimmungen</h1>
+
+<p>(1) Rechtserhebliche Erklärungen und Anzeigen, die nach Vertragsschluss abzugeben sind, bedürfen zu ihrer Wirksamkeit der Schriftform.</p>
+
+<p>(2) Diese Vertragsbedingungen gelten ausschließlich. Anderweitigen Geschäftsbedingungen des Anbieters, des Kunden oder Dritter wird hier hiermit ausdrücklich widersprochen.</p>
+
+<p>(3) Mündliche Nebenabreden bestehen nicht. Änderungen, Ergänzungen und die Aufhebung dieses Vertrages bedürfen der Schriftform. Dies gilt auch für die Änderung dieser Schriftformklausel selbst.</p>
+
+<p>(4) Sollten einzelne Bestimmungen dieses Vertrages ganz oder teilweise unwirksam sein oder nach Vertragsschluss unwirksam werden, so wird die Wirksamkeit der übrigen Bestimmungen hierdurch nicht berührt.</p>
+<p>Die Vertragsparteien sind in diesem Fall verpflichtet, über eine wirksame und zumutbare Ersatzregelung zu verhandeln, die dem mit der unwirksamen Bestimmung verfolgten Sinn und Zweck möglichst nahe kommt. Dies gilt auch im Falle einer Vertragslücke.</p>
+
+<p>(5) Anhänge zu diesem Vertrag sind Bestandteil dieses Vertrags.</p>
+
+<p>(6) Dieser Vertrag unterliegt ausschließlich dem Recht der Bundesrepublik Deutschland.</p>
+
+<p>(7) Der Gerichtsstand für alle Streitigkeiten aus diesem Vertrag bestimmt sich nach den maßgeblichen Regeln der ZPO.</p>
---
+++ license_short.php
@@ -0,0 +1,11 @@
+<p>Die volle Version bekommen Sie <a href="license.php">hier</a>.</p>
+
+<p>Die Kurzversion ist: wir versuchen zuvorkommend zu sein. Bei Problemen, auch bei Zahlungsproblemen, können Sie sich melden und wir sind kulant.</p>
+
+<p>Sie können jederzeit formlos kündigen und es folgen keine Konsequenzen. Sie können jederzeit zurückkehren, wenn Sie vorher ein Datenbank-Backup gemacht haben (wovon wir Sie dann in Kenntnis setzen), als wäre nichts gewesen. Wir löschen wir Ihre Daten nach bis zu 48 Stunden.</p>
+
+<p>Wir garantieren keine 100%ige Uptime. Die Software wird dauerhaft weiterentwickelt ("<a href="https://de.wikipedia.org/wiki/Rolling_Release">Rolling Release</a>"), daher kann es zu kurzen Downtimes kommen. Wir halten sie aber so gering wie möglich.</p>
+
+<p>Und natürlich verpflichten Sie sich, nichts Illegales oder gegen das Urheberrecht verstoßes hochzuladen. Sie sind dafür verantwortlich, dass Sie und ihre Dozenten (und Sonstige, die befugt sind, auf die Software zuzugreifen) sich daran halten.</p>
+
+<p>Wir arbeiten an einer stetigen Erweiterung unserer Schulungsvideos, so dass alle Themenbereiche von einem Video abgedeckt sind.</p>
