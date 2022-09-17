893668c01d4b6e0845252b6bc2291fa081499f36
vvz-test <kochnorman@rocketmail.com>
2022-09-17 15:57:30 +0200

    fixed installer?

--- install.sh
+++ install.sh
@@ -45,14 +45,10 @@ while [[ -z "$PASSWORD" ]]; do
 done
 
 cd $INSTALL_PATH
-if [ -d "$INSTALL_PATH/../.git" ]; then
-	git pull
-else
-	git clone --depth 1 https://github.com/NormanTUD/VVZ.git .
-	git config --global user.name "$(hostname)"
-	git config --global user.email "kochnorman@rocketmail.com"
-	git config pull.rebase false
-fi
+git clone --depth 1 https://github.com/NormanTUD/VVZ.git .
+git config --global user.name "$(hostname)"
+git config --global user.email "kochnorman@rocketmail.com"
+git config pull.rebase false
 cd -
 
 
