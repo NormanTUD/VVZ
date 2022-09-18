6130caea8e5d5852a893b21999d5d8f7dd7247ed
vvz-test <kochnorman@rocketmail.com>
2022-09-18 15:26:25 +0200

    fix

--- pages/welcome.php
+++ pages/welcome.php
@@ -38,7 +38,7 @@
 <?php
 				if(check_page_rights(get_page_id_by_filename("neuerdozent.php"))) {
 ?>
-					<li><a href="admin?page=<?php print get_page_id_by_filename("neuerdozent.php"); ?>"><?php print_person_icon(); ?> Dozenten hinzufügen</a></li>
+					<li><a href="admin?page=<?php print get_page_id_by_filename("neuerdozent.php"); ?>"><span class='utf8symbol'><?php print_person_icon(); ?></span> Dozenten hinzufügen</a></li>
 <?php
 				}
 
