38d6782b500ffb7cba3d3c9d7e75f90ec24d7677
vvz-test <kochnorman@rocketmail.com>
2022-09-18 13:55:05 +0200

    fix

--- startseite.php
+++ startseite.php
@@ -21,10 +21,8 @@
 			"bootstrap-tour-standalone.css",
 			"jquery-ui-timepicker-addon.css",
 			"custom.php",
-			//"font-awesome.min.css"
+			"font-awesome.min.css"
 		));
-
-		js("f87f2e7625.js");
 ?>
 
 		<!-- Container element -->
