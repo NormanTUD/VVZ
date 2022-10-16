9afd119b56fac63982ccc426e37e450a1f4f9648
vvz-test <kochnorman@rocketmail.com>
2022-10-16 18:11:13 +0200

    delete

---
+++ delete_from_all_dbs.php
@@ -0,0 +1,26 @@
+<?php
+	$GLOBALS["no_selftest_force"] = 1;
+	include("functions.php");
+	function isCommandLineInterface() {
+		return (php_sapi_name() === 'cli');
+	}
+
+	if(isCommandLineInterface()) {
+		rquery('SET foreign_key_checks = 0');
+		$query = "show tables from vvztud";
+		$result = rquery($query);
+		while ($row = mysqli_fetch_row($result)) {
+			if(!preg_match("/^view_/", $row[0])) {
+				print "Deleting all from '$row[0]'\n";
+				try {
+					rquery("delete from $row[0];");
+				} catch (\Throwable $e) {
+					print $e;
+				}
+			}
+		}
+		rquery('SET foreign_key_checks = 1');
+	} else {
+		die("Can only be called from the CLI.");
+	}
+?>
