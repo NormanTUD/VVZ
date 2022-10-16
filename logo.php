f8cac06614fade333b9a04597c8712042c29ad06
vvz-test <kochnorman@rocketmail.com>
2022-10-16 17:38:29 +0200

    fixed logo

--- logo.php
+++ logo.php
@@ -4,26 +4,31 @@
 	include_once("functions.php");
 	include_once("selftest.php");
 
-	header("Content-type: image/png");
+	#header("Content-type: image/png");
 
 	$id = get_kunde_id_by_db_name($GLOBALS["dbname"]);
 	if(get_get("kunde_id")) {
 		$id = get_get("kunde_id");
 	}
 
-	if(file_exists("/etc/vvztud")) {
-		header('Content-type: image/svg+xml');
-		readfile("tudlogo.svg");
-	} else {
-		if($id) {
-			$query = "select img from vvz_global.logos where kunde_id = ".esc($id);
-			$result = get_single_row_from_query($query);
+	if($id) {
+		$query = "select img from vvz_global.logos where kunde_id = ".esc($id);
+		$result = get_single_row_from_query($query);
 
-			if($result) {
-				print $result;
+		if($result) {
+			print $result;
+		} else {
+			if(file_exists("/etc/vvztud")) {
+				header('Content-type: image/svg+xml');
+				readfile("tudlogo.svg");
 			} else {
 				readfile("default_logo.png");
 			}
+		}
+	} else {
+		if(file_exists("/etc/vvztud")) {
+			header('Content-type: image/svg+xml');
+			readfile("tudlogo.svg");
 		} else {
 			readfile("default_logo.png");
 		}
