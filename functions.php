f75a7a85a62ed219d0a4b6fc925c306841e35c9c
vvz-test <kochnorman@rocketmail.com>
2022-12-03 18:36:55 +0100

    fix

--- functions.php
+++ functions.php
@@ -10690,7 +10690,7 @@ order by
 			$value = 0;
 		}
 
-		$query = "insert into sperrvermerk (enabled, semester_id) values ($value, ".esc($semester_id).") on duplicate key update enabled enabled=values(enabled)";
+		$query = "insert into sperrvermerk (enabled, semester_id) values ($value, ".esc($semester_id).") on duplicate key update enabled=values(enabled)";
 
 		return rquery($query);
 	}
