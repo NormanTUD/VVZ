b13e956035305c8832cca44d6b27ed50f5836b3a
vvz-test <kochnorman@rocketmail.com>
2022-12-03 17:34:07 +0100

    sperrvermerk

---
+++ pages/sperrvermerk.php
@@ -0,0 +1,14 @@
+<?php
+	$included_files = get_included_files();
+	$included_files = array_map('basename', $included_files);
+
+	if(!in_array('functions.php', $included_files)) {
+		include_once('../functions.php');
+	}
+
+	if(check_page_rights(get_page_id_by_filename(basename(__FILE__)))) { // Wichtig, damit Niemand ohne Anmeldung etwas ändern kann
+?>
+		
+<?php
+	}
+?>
